6a8f611245a1ec8ab50218aa2902867cfa52cf1e
DongjinJ <42710922+DongjinJ@users.noreply.github.com>
2021-07-06 17:16:44 +0900

    Update cv_test.py
    - OpenCV 를 이용해 Cam Image 를 GUI 상에 띄우는 예제

--- Whale_mk1_Rasp/cv_test.py
+++ Whale_mk1_Rasp/cv_test.py
@@ -1,9 +1,52 @@
+from PyQt5 import QtGui
 import cv2
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
 
-capture = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = capture.read()
-    cv2.imshow('image', frame)
+class MyApp(QWidget):
+    capture = cv2.VideoCapture(0)
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        self.image_region = QLabel(self)
+        self.image_region.move(5, 5)
+        
+        self.timer = QTimer(self)
+        self.timer.setInterval(int(1/60*1000))
+        self.timer.timeout.connect(self.updateFrame)
+
+        self.timer.start()
+        self.setWindowTitle('WebCam')
+        self.resize(600, 340)
+        self.show()
+
+    def updateFrame(self):
+        while True:
+            ret, frame = self.capture.read()
+            frame = cv2.resize(frame, dsize=(320,240), interpolation=cv2.INTER_AREA)
+            color_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, c = frame.shape
+            self.image_region.resize(w, h)
+            qt_frame = QImage(color_image.data,
+                            w,
+                            h,
+                            w*c,
+                            QImage.Format_RGB888)
+            pixmap = QtGui.QPixmap.fromImage(qt_frame)
+            self.image_region.setPixmap(pixmap)
+            if cv2.waitKey(1) == ord('q'):
+                self.capture.release()
+                break
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
